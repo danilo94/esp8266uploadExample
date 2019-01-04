7b5d4c571fb80fcacb94be2ea55d2cb5636a1c2f
Danilo Almeida <danilooalmeida94@gmail.com>
2019-01-03 22:06:09 -0200

    Add files via upload

---
+++ testecarregamento.ino
@@ -0,0 +1,13 @@
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Serial foi Ativada");
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  Serial.println("O código foi carregado com sucesso");
+  delay(1000);
+  // put your main code here, to run repeatedly:
+
+}
